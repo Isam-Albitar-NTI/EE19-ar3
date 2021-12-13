eddf9788cdc335e0066a5a4677a7463a7007e6cc
Isam-AlBitar-NTI <isam.albitar@elev.ga.ntig.se>
2021-12-13 10:37:21 +0100

    arbetat med madlib, rand(), array och php

--- madlibs/madlib.php
+++ madlibs/madlib.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="sv">
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -13,16 +12,29 @@
     <div class="kontainer">
         <h1 class="display-4">Madlibs</h1>
         <?php
-        $namn = filter_input(INPUT_POST, 'namn', FILTER_SANITIZE_STRING);
-        $adjektiv1 = filter_input(INPUT_POST, 'adjektiv1', FILTER_SANITIZE_STRING);
-        $adjektiv2 = filter_input(INPUT_POST, 'adjektiv2', FILTER_SANITIZE_STRING);
-        $adjektiv3 = filter_input(INPUT_POST, 'adjektiv3', FILTER_SANITIZE_STRING);
-        $substantiv = filter_input(INPUT_POST, 'substantiv', FILTER_SANITIZE_STRING);
-        $verb = filter_input(INPUT_POST, 'verb', FILTER_SANITIZE_STRING);
-        // Skriv ut madlibs-texten
-        echo "Du heter $namn. Du märker din högra hand är $adjektiv1 och din vänstra hand är $adjektiv2. Din ögon färg är $adjektiv3. Du har också en objekt som hänger runt din hals, det är $substantiv. Du märker också något som du inte hade märkt innan, du $verb.";
-        ?>
+        $adjective = filter_input(INPUT_POST, 'adjective', FILTER_SANITIZE_STRING);
+        $verbIng = filter_input(INPUT_POST, 'verbIng', FILTER_SANITIZE_STRING);
+        $food = filter_input(INPUT_POST, 'food', FILTER_SANITIZE_STRING);
+        $noun = filter_input(INPUT_POST, 'noun', FILTER_SANITIZE_STRING);
+        $number = filter_input(INPUT_POST, 'number', FILTER_SANITIZE_STRING);
+        $bodyPart = filter_input(INPUT_POST, 'bodyPart', FILTER_SANITIZE_STRING);
+
+        // Innehåller variablerna text då skriver vi ut madlibs
+        if ($adjective && $verbIng && $food && $noun && $number && $bodyPart) {
+            // Lager av olika verb
+            $verben = ["eat", "sleep", "jump", "swim", "clean", "sing", "drive", "read", "watch", "yell"];
+            
+            // Slumpa fram ett verb
+            
+
+            // Skriv ut madlibs-texten
+            echo "<p><strong>Mario:</strong> What a/an $adjective day, eh Luigi The perfect day for $verbIng some Goombas. The $food Kingdom is crawling with them!</p>
+            <p><strong>Luigi:</strong> You're right about that, dear $noun. Hope you're ready to " . $verben[rand(0, 11)] . ".</p>
+            <p><strong>Mario:</strong> Ready? I've waited $number years to " . $verben[rand(0, 11)]. " that scoundrel Bowser!</p>
+            <p><strong>Luigi:</strong> Pipe down. he has $bodyPart everywhere.
+            <p><strong>Mario:</strong> First I'll " . $verben[rand(0, 11)] . " and grab a/an $food. That'll give me $noun!";
+        }
+            ?>
     </div>
 </body>
-
 </html>
