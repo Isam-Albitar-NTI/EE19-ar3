57bbc3c9e26762d5a4c8bc1e06cbf6978ac49822
Isam-AlBitar-NTI <isam.albitar@elev.ga.ntig.se>
2022-04-26 13:27:18 +0200

    Arbetat mer med class, gjorde private class & med validator

---
+++ OOP/Validator/form.php
@@ -0,0 +1,54 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// Ladda in klassen
+include "./user-validator.php";
+
+// Kolla om data skickats
+
+$username = filter_input(INPUT_POST, "username");
+$email = filter_input(INPUT_POST, "email");
+
+if ($username && $email) {
+    //echo "<p>$username && $email</p>";
+
+    // Användd klassen Validator
+    $controll = new Validator($_POST);
+    $controll->ValidateUsername(); 
+    $controll->ValidateEmail();
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="sv">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="kontainer">
+        <h1>Ett formulär</h1>
+
+        <form action="#" method="post">
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Username</label>
+                <input type="text" class="form-control" id="exampleFormControlInput1" name="username">
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Username</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" name="email">
+            </div>
+            <button type="submit" class="btn btn-primary">Primary</button>
+        </div>
+    </form>
+</body>
+
+</html>
---
+++ OOP/Validator/user-validator.php
@@ -0,0 +1,29 @@
+<?php
+class Validator {
+
+    // Egenskaper
+    private $data;
+
+    public function __construct($postData)
+    {
+        $this->data = $postData;
+    }
+
+    // Metoderna 
+
+    public function ValidateUsername() {
+        // echo "<p>{$this->data['username']}</p>";
+        // Kolla längd 5-15 tecken
+        if (strlen($this->data['username']) < 5 || strlen($this->data['username']) > 15) {
+            echo "<p>Användarnamnet måste vara 5-15 tecken</p>";
+        }
+    }
+
+    public function ValidateEmail() {
+        //echo "<p>{$this->data['email']}</p>";
+        if (strlen($this->data['email']) < 10 || strlen($this->data['email']) > 25) {
+            echo "<p>Email måste vara 10-25 tecken</p>";
+        }
+    }
+}
+?>
